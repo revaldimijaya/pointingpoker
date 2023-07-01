1434d37a7a45c1850ec42f9c219e46dabe6a3c7d
revaldimijayatkp <revaldi.mijaya@tokopedia.com>
2023-07-01 17:51:22 +0700

    add logic for pointing poker

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,52 +1,131 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import './homepage.css'
 
 const socket = io.connect('http://localhost:4000'); // Replace with your Socket.IO server URL
 
 function Homepage() {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
+  const [room, setRoom] = useState('');
+  const [name, setName] = useState('');
+  const [participants, setParticipants] = useState([]);
+  const [pointingValue, setPointingValue] = useState('');
 
   useEffect(() => {
-    socket.on('message', (message) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
+    socket.on('participants', (value) => {
+      setParticipants(value)
     });
+
+    return () => {
+      console.log("discconnect")
+      socket.disconnect();
+    };
   }, []);
 
   const handleMessageChange = (event) => {
-    setMessage(event.target.value);
+    setPointingValue(event.target.value);
   };
+
+  const handleJoinRoomChange = (event) => {
+    setRoom(event.target.value)
+  }
+
+  const handleNameChange = (event) => {
+    setName(event.target.value)
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    socket.emit('message', message);
-    setMessage('');
+    socket.emit('message', {id: socket.id, room: room, pointingValue: pointingValue});
+  };
+
+  const handleJoinRoom = (event) => {
+    event.preventDefault();
+    socket.emit('joinRoom', { room: room, name: name, value: 0 });
   };
 
   return (
     <Container className="mt-5">
       <Row>
-        <Col md={6} className="offset-md-3">
-          <h3 className="mb-3">Chat App</h3>
-          <div className="messages">
-            {messages.map((msg, index) => (
-              <div key={index} className="message">
-                {msg}
-              </div>
-            ))}
-          </div>
-          <Form onSubmit={handleSubmit}>
+        <h3 className="mb-3">Pointing Poker</h3>
+      </Row>
+      <Row>
+        <div className="join-room">
+          <Form onSubmit={handleJoinRoom}>
             <Form.Group>
               <Form.Control
                 type="text"
-                placeholder="Type a message"
-                value={message}
-                onChange={handleMessageChange}
+                placeholder="name"
+                value={name}
+                onChange={handleNameChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                placeholder="room 1"
+                value={room}
+                onChange={handleJoinRoomChange}
               />
             </Form.Group>
             <Button variant="primary" type="submit">
               Send
+            </Button>
+          </Form>
+        </div>
+      </Row>
+      <br>
+      </br>
+      <Row>
+        <h4 className="mb-3">Participants</h4>
+        <Col md={6} className="offset-md-3">
+          <div className="messages">
+            {participants.map((value,  index) => (
+              <div key={index} className="message">
+                <Col>
+
+                </Col>
+                {value.id} {value.name} {value.value}
+              </div>
+            ))}
+          </div>
+        </Col>
+      </Row>
+      <br>
+      </br>
+      <Row>
+        <h4 className="mb-3">Pick your poison</h4>
+        <Col md={6} className="offset-md-3">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Control className='pointing-value'
+                type="button"
+                value={"1"}
+                onClick={handleMessageChange}
+              />
+              <Form.Control className='pointing-value'
+                type="button"
+                value={"2"}
+                onClick={handleMessageChange}
+              />
+              <Form.Control className='pointing-value'
+                type="button"
+                value={"5"}
+                onClick={handleMessageChange}
+              />
+              <Form.Control className='pointing-value'
+                type="button"
+                value={"8"}
+                onClick={handleMessageChange}
+              />
+              <Form.Control className='pointing-value'
+                type="button"
+                value={"12"}
+                onClick={handleMessageChange}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
             </Button>
           </Form>
         </Col>
--- src/server.js
+++ src/server.js
@@ -1,21 +1,72 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, { cors: { origin: '*' } });
-// const cors = require('cors');
+
+const rooms = new Map(); // Map to store participants in each room
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('A user connected ', socket.id);
 
   socket.on('message', (message) => {
     console.log('Received message:', message);
-    io.emit('message', message);
+    let room = rooms.get(message.room)
+    room.forEach(participant => {
+      console.log("participant: ", participant)
+      if (participant.id === message.id) {
+        participant.value = message.pointingValue;
+        console.log("found : ",room)
+      }
+    })
+
+    console.log("room: ", room)
+    const participants = Array.from(room).map(participant => participant);
+    socket.emit('participants', participants);
+    socket.to(message.room).emit('participants', participants)
+  });
+
+  socket.on('joinRoom', ({ room, name, value }) => {
+    socket.join(room);
+
+    // Add participant to the room
+    if (!rooms.has(room)) {
+      rooms.set(room, new Set());
+    }
+    rooms.get(room).add({ id: socket.id, name, value });
+    // console.log(rooms)
+
+    // Get the participant in the room
+    const participants = Array.from(rooms.get(room)).map(participant => participant);
+
+    socket.emit('participants', participants);
+    socket.to(room).emit('participants', participants);
   });
 
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    // Remove participant from the room
+    console.log("user disconnected ", socket.id)
+    let selectedRoom
+    rooms.forEach((participants, room) => {
+      // console.log("line 1: ", participants, room)
+      participants.forEach(participant => {
+        // console.log("line 2: ", participant)
+        if (participant.id === socket.id) {
+          console.log("found : ", participant)
+          participants.delete(participant);
+          console.log("sisa : ", participants)
+          selectedRoom = room
+        }
+      });
+    });
+
+    if (selectedRoom) {
+      // Get the participant names in the room
+      console.log("selected room: ", selectedRoom)
+      const participants = Array.from(rooms.get(selectedRoom) || []).map(participantsLeft => participantsLeft);
+      console.log(participants)
+      socket.to(selectedRoom).emit('participants', participants);
+    }
   });
 });
-
 
 http.listen(4000, () => {
   console.log('Server is running on http://localhost:4000');
